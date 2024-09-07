29fe36207a7560219c99b8d6a59e781788b9b59a
anupamababu1984 <158604256+anupamababu1984@users.noreply.github.com>
2024-09-07 15:42:25 -0500

    Update reverse.py

--- Day-01/reverse.py
+++ Day-01/reverse.py
@@ -3,8 +3,9 @@ num = int(input("Enter a num: " ))
 def revNumber(num):
   reverse = 0
   while num != 0:
-    reverse = (reverse*0) + (num%10)
+    reverse = (reverse*10) + (num%10)
     num = num//10
   return reverse
 
 print("The reverse of the number : " , revNumber(num))  
+        
